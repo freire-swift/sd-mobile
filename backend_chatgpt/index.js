09e920c103223f7727cbd426bba186335faa4268
Pedro Henrique Freire de Oliveira <pedrobolina123@gmail.com>
2024-10-08 20:30:01 -0300

    commit 2

--- backend_chatgpt/index.js
+++ backend_chatgpt/index.js
@@ -1,10 +1,17 @@
 require('dotenv').config()
+//importamos o express
 const express = require('express')
+//construímos o objeto que viabiliza a especificação de endpoints
 const app = express()
-const string = 
-
-
-
-
+//aplicamos o middleware de transformação JSON
+app.use(express.json())
 const OPENAI_API_KEY = process.env.OPENAI_API_KEY
-console.log(OPENAI_API_KEY)
+//especificamos o endpoint de interesse
+//POST /pergunte-ao-chatgpt
+app.post('/pergunte-ao-chatgpt', (req, res) => {
+//respondemos um 'ok' só para testar
+res.send('ok')
+})
+//colocamos o servidor em execução na porta 4000
+const PORT = 4000
+app.listen(PORT, () => console.log(`Em execução na porta ${PORT}`))
