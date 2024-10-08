ae26a7809048c5aa0305acb13f1be00d242c5e9a
Pedro Henrique Freire de Oliveira <pedrobolina123@gmail.com>
2024-10-08 20:26:53 -0300

    first commit

---
+++ backend_chatgpt/index.js
@@ -0,0 +1,10 @@
+require('dotenv').config()
+const express = require('express')
+const app = express()
+const string = 
+
+
+
+
+const OPENAI_API_KEY = process.env.OPENAI_API_KEY
+console.log(OPENAI_API_KEY)
