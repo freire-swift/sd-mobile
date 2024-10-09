7aa7ef726507345a6b89d5bbf7e180503df94528
Pedro Henrique Freire de Oliveira <pedrobolina123@gmail.com>
2024-10-08 21:39:25 -0300

    cria endpoints

--- backend_chatgpt/index.js
+++ backend_chatgpt/index.js
@@ -1,6 +1,8 @@
 require('dotenv').config()
 //importamos o express
 const express = require('express')
+
+const { OpenAI } = require('openai')
 //construímos o objeto que viabiliza a especificação de endpoints
 const app = express()
 //aplicamos o middleware de transformação JSON
@@ -9,9 +11,17 @@ const OPENAI_API_KEY = process.env.OPENAI_API_KEY
 //especificamos o endpoint de interesse
 //POST /pergunte-ao-chatgpt
 app.post('/pergunte-ao-chatgpt', (req, res) => {
-//respondemos um 'ok' só para testar
-res.send('ok')
+    const openai = new OpenAI(OPENAI_API_KEY)
+    res.json({mensagem: 'OK'})
 })
-//colocamos o servidor em execução na porta 4000
-const PORT = 4000
-app.listen(PORT, () => console.log(`Em execução na porta ${PORT}`))
+
+app.get('/oi', (req, res) => {
+    let nome = req.query.nome
+    res.json({msg: `Oi, ${req.query.nome}`})
+})
+
+//colocamos o servidor em execução na porta 3000
+const PORT = 3000
+app.listen(PORT, () => {
+    console.log(`Em execução na porta ${PORT}`)
+})
